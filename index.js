256a6d8f867c7356ef32158c7d53aa8905e30e2c
andres felipe agudelo castro <71729410+afagudelo94@users.noreply.github.com>
2023-09-10 21:02:53 -0500

    eliminando funcion
    probando eliminando funcion

---
+++ index.js
@@ -0,0 +1,105 @@
+const actividadUI = document.querySelector('#actividad')
+const descripcionUI = document.querySelector('#descripcion')
+const botonEnviarUI = document.querySelector('#enviar')
+const sectionMostrar = document.querySelector('#mostrar')
+const botonActualizarUI = document.querySelector('#actualizar')
+ 
+let referencia = ''
+ 
+botonEnviarUI.addEventListener('click', addToDo)
+botonActualizarUI.addEventListener('click', editar2)
+document.addEventListener('DOMContentLoaded', mantener)
+ 
+
+function addToDo(event){
+    event.preventDefault()
+    let datos = JSON.parse(localStorage.getItem('elements'))
+ 
+    if(datos == null){
+        datos = []
+    }
+ 
+    const objetoTemporal={
+        actividad: actividadUI.value,
+        descripcion: descripcionUI.value
+    }
+
+    datos.push(objetoTemporal)
+
+    localStorage.setItem('elements', JSON.stringify(datos))
+   
+    imprimir()
+
+}
+
+function imprimir(){
+    let datos = JSON.parse(localStorage.getItem('elements'))
+
+    sectionMostrar.innerHTML = datos.map((dato, index) =>{
+        return /*HTML*/`
+
+        <div class='card'>
+            <h3>${dato.actividad}</h3>
+            <h3>${dato.descripcion}</h3>
+            <button onClick="editar(${index})">editar</button>
+            <button onClick="borrar(${index})">Eliminar</button>
+        </div>
+
+        `
+    }).join('<hr />')
+
+}
+
+function editar(index){
+    let datos = JSON.parse(localStorage.getItem('elements'))
+    actividadUI.value = datos[index].actividad
+    descripcionUI.value = datos[index].descripcion
+ 
+    botonActualizarUI.style.display = "block"
+    botonEnviarUI.style.display = "none"
+ 
+    referencia = index
+
+}
+
+function editar2(event){
+    event.preventDefault()
+
+    let datos = JSON.parse(localStorage.getItem('elements'))
+
+    const objetoTemporal={
+        actividad: actividadUI.value,
+        descripcion: descripcionUI.value
+    }
+    
+    datos.splice(referencia, 1, objetoTemporal)
+
+    localStorage.setItem('elements', JSON.stringify(datos))
+
+    imprimir(datos)
+
+    descripcionUI.value = ""
+    actividadUI.value = ""
+
+    botonActualizarUI.style.display = "none"
+    botonEnviarUI.style.display = "block"
+
+}
+
+function borrar(index){
+    const array = localStorage.getItem('elements')
+
+        const actividadArray = JSON.parse(array)
+        actividadArray.splice(index,1)
+
+    localStorage.setItem('elements', JSON.stringify(actividadArray))
+    imprimir(actividadArray)
+}
+/*
+function mantener(){
+    const datos = JSON.parse(localStorage.getItem('elements'))
+
+    if (datos) {
+        imprimir(datos)
+    }
+}*/
